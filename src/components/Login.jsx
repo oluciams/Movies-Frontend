dc05eea17d5f8cdd7d2f3805ed15755f9605d90b
olga <oluciams84@gmail.com>
2022-09-22 18:28:43 -0500

    add validations

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
 
 export const Login = () => {
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    console.log( regexEmail.test(email) );
+
+    if(email === '' || password === ''){
+      console.log('Input vacios');
+      return;
+    }
+    
+    if(email !== '' && !regexEmail.test(email)) {
+      console.log('Correo invalido');
+      return;
+    }
+
+    if(email !== 'challenge@alkemy.org' || password !== 'react'){
+      console.log('Credenciales inválidas');
+      return;
+    }
+  }
+
   return (
     <>
       <h2>Formulario de Login</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Correo Electronico:</span><br />
-          <input type="email" name="email" />
+          <input type="text" name="email" />
         </label>
         <br />
         <label>
