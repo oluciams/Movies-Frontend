44da36457f1b4986aed0831591cb8de744040021
olga <oluciams84@gmail.com>
2022-09-22 17:10:51 -0500

    initiall commit and login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <>
+      <h2>Formulario de Login</h2>
+      <form>
+        <label>
+          <span>Correo Electronico:</span><br />
+          <input type="email" name="email" />
+        </label>
+        <br />
+        <label>
+          <span>Contraseña</span><br />
+          <input type="password" name="password" />
+        </label>
+        <br />                                                          
+        <button type="submit">Ingresar</button>
+      </form>
+    </>
+  )
+}
