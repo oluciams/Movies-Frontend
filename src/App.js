87544833d9c3b342bc0513c7aa86fd5ca2af13c1
olga <oluciams84@gmail.com>
2022-09-23 19:11:51 -0500

    add header and footer component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { Route, Routes } from "react-router-dom";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 import { Listado } from "./components/Listado";
 import { Login } from "./components/Login";
 
 function App() {
   return (
+  <>
+    <Header/>
     <Routes>
     <Route path="/" element={<Login />} />
-    <Route path="listado" element={<Listado />} />   
-    </Routes>  
+    <Route path="listado" element={<Listado />} /> 
+    <Route path="listado" element={<Header />} />   
+    </Routes>
+    <Footer/>  
+  </>
     
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return(
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="http://instagram.com" rel="noopener noreferrer">Instagram</a>
+          </li>
+        </ul>
+      </nav>
+      <p>Copyright Alkemy</p>
+    </footer>
+  )
+}
