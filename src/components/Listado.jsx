340e49328bbf0280d8288ab225d0f946f044ed64
olga <oluciams84@gmail.com>
2022-09-24 19:56:38 -0500

    add catch error

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import Swal from 'sweetalert2';
 
 
 export const Listado = ()=> { 
@@ -10,11 +11,18 @@ export const Listado = ()=> {
 
   const getData = () => {
     const endPoint = "https://api.themoviedb.org/3/discover/movie?api_key=b61364da3cf775b0c719a30ee4fe39b2&language=es-ES&page=1";
-    axios.get(endPoint)
-      .then(res => {
-        const apiData = res.data
-        setMoviesList(apiData.results)  
-      })   
+      axios.get(endPoint)
+        .then(res => {
+          const apiData = res.data
+          setMoviesList(apiData.results)  
+        })  
+        .catch (error => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Alert',
+            html: '<p>Ocurrio un error</p>'
+          })    
+        })
   }
 
   useEffect(() => {
@@ -26,11 +34,11 @@ export const Listado = ()=> {
   return (     
     <div className="row">
       {
-        moviesList.map(({idx, title, poster_path, overview })=>{
+        moviesList.map(({ title, poster_path, overview }, idx )=>{
           return(
             <div className="col-3" key={idx}>
               <div className="card my-4" style={{width: "18rem"}}>
-                <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} className="card-img-top" alt="..."/>
+                <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} className="card-img-top" alt="movie"/>
                 <div className="card-body">
                   <h5 className="card-title">{title.substring(0, 20)}</h5>
                   <p className="card-text">{overview.substring(0, 50)}....</p>
